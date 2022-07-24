b61bc764ed7f48c09bd49cf2f3210c3e364644d7
CanerAyyildiz <canerayyildiz@gmail.com>
2022-07-24 18:50:28 +0300

    Added constructor package

---
+++ src/constructors/Employee.java
@@ -0,0 +1,19 @@
+package constructors;
+
+public class Employee {
+
+
+
+
+
+  /*  Task 1:
+    Employee:
+    attribute/fields/variables
+            name, jobTitle, ID, salary
+
+    add a constructor to initialize all the fields
+    add a default constructor with no fields
+
+    toString() method will rint out the fields
+ */
+ }
