7bb4f75184b58b899afdcb9e29a1dbd2fb160b13
CanerAyyildiz <canerayyildiz@gmail.com>
2022-07-24 18:57:16 +0300

    created employee class

--- src/constructors/Employee.java
+++ src/constructors/Employee.java
@@ -2,6 +2,26 @@ package constructors;
 
 public class Employee {
 
+ public class Employee {
+  public String name,jobTitle;
+  public int ID;
+  public double salary;             // instance variable, belongs to Object
+  public static String companyName; // static field belongs to Class
+
+  static {
+   companyName="Microsoft";
+  }
+
+  public Employee() {
+   salary = 10000;
+  }
+
+  public Employee(String n, String jobTitle, int ID, double salary) {
+   name = n;
+   this.jobTitle = jobTitle;
+   this.ID = ID;
+   this.salary = salary;
+  }}
 
 
 
